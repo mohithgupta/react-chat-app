8d65df5897f756aedeee05ca3868852cb93849d1
You Know Who <you@example.commohithguptak@gmail.com>
2021-03-18 13:27:14 +0530

    added test credntials

--- src/index.js
+++ src/index.js
@@ -3,4 +3,6 @@ import ReactDOM from 'react-dom';
 
 import App from './App';
 
+alert("Please use the following credentials. UserName : test , Password : test123")
+
 ReactDOM.render(<App />, document.getElementById('root'));
